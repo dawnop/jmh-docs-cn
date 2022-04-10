25b0b467a8893a002bb5ec30fc4fe7bca3405d9f
dawn <wyfdggssld@126.com>
2022-04-10 21:27:16 +0800

    translate to Chinese

--- src/main/java/com/dawnop/benchmark/JMHSample_02_BenchmarkModes.java
+++ src/main/java/com/dawnop/benchmark/JMHSample_02_BenchmarkModes.java
@@ -13,6 +13,22 @@ import java.util.concurrent.TimeUnit;
 
 public class JMHSample_02_BenchmarkModes {
 
+
+    /**
+     * JMH 在编译期间生成代码。JMH 可以通过多种 mode 对代码进行基准测试。
+     * 通过 {@link BenchmarkMode} 指定默认的 mode。还可以在 main 函数中显式覆写默认 mode。
+     * <p>
+     * 执行的方法可能抛出异常，可以显式声明抛出异常。
+     * 一旦真的抛出异常，基准测试将终止。
+     * <p>
+     * 当你对本框架的某种 behavior 产生困惑时，看看本框架生成的代码会很有帮助。
+     * 生成的代码可能不是按照你预想的方式在运行。
+     * 建议按照教程的顺序进行，并且记得检查生成的代码。
+     * <p>
+     * 这个 Java 文件生成的代码的位置是：
+     * target/generated-sources/annotations/.../JMHSample_02_BenchmarkModes.java
+     */
+
     public static void main(String[] args) throws RunnerException {
         Options options = new OptionsBuilder()
                 .include(JMHSample_02_BenchmarkModes.class.getSimpleName())
@@ -25,30 +41,12 @@ public class JMHSample_02_BenchmarkModes {
         new Runner(options).run();
     }
 
-
-    /*
-     * JMH 在编译期间生成代码。JMH 可以通过多种 mode 对代码进行基准测试。
-     * 通过 @BenchmarkMode 指定默认的 mode。还可以在 main 函数中显式覆写默认 mode。
-     *
-     * 执行的方法可能抛出异常，可以显式声明抛出异常。
-     * 一旦真的抛出异常，基准测试将终止。
-     *
-     * 当你对本框架的某种 behavior 产生困惑时，看看本框架生成的代码会很有帮助。
-     * 生成的代码可能不是按照你预想的方式在运行。
-     * 建议按照教程的顺序进行，并且记得检查生成的代码。
-     *
-     * 这个 Java 文件生成的代码的位置是：
-     * target/generated-sources/annotations/.../JMHSample_02_BenchmarkModes.java
-     */
-
     /**
-     * <p>
      * Throughput: 每单位时间的操作 ops/time
-     * </p><p>
+     * <p>
      * {@link Mode#Throughput}：
      * 持续调用标记有{@link Benchmark}的方法，计算所有工作线程的总吞吐量。
      * 在一定的时间内反复调用，直至时间耗尽。
-     * </p>
      */
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
@@ -57,22 +55,13 @@ public class JMHSample_02_BenchmarkModes {
         TimeUnit.MILLISECONDS.sleep(100);
     }
 
-    /*
-     * Mode.AverageTime measures the average execution time, and it does it
-     * in the way similar to Mode.Throughput.
-     *
-     * Some might say it is the reciprocal throughput, and it really is.
-     * There are workloads where measuring times is more convenient though.
-     */
 
     /**
-     * <p>
      * Average time: 每次操作的平均时间 time/op
-     * </p><p>
+     * <p>
      * {@link Mode#AverageTime}：
      * 持续调用标记有{@link Benchmark}的方法，计算调用所有工作线程的平均时间。
      * 这个是{@link Mode#Throughput}的倒数，在一定的时间内反复调用，直至时间耗尽。
-     * </p>
      */
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
@@ -82,14 +71,12 @@ public class JMHSample_02_BenchmarkModes {
     }
 
     /**
-     * <p>
      * Sample time: 对每次操作的时间进行采样 Sampling time
-     * </p><p>
+     * <p>
      * {@link Mode#SampleTime}：
      * 持续调用标记有{@link Benchmark}的方法，随机采集一部分测试需要的时间。
      * 可以采集运行时间的分布，百分比等。这个模式自动调整采样频率，但是可能会因为测量的暂停遗漏一些采样。
      * 在一定的时间内反复调用，直至时间耗尽。
-     * </p>
      */
     @Benchmark
     @BenchmarkMode(Mode.SampleTime)
@@ -99,13 +86,12 @@ public class JMHSample_02_BenchmarkModes {
     }
 
     /**
-     * <p>
      * Single shot time: 测量单次操作的时间。
-     * </p><p>
+     * <p>
      * {@link Mode#SingleShotTime}：
      * 运行一次{@link Benchmark}方法，并且测量它的时间。
      * 不进行 warm up（不进行 JIT 优化）
-     * </p>
+     * <p>
      * 这种模式的注意事项包括:
      * <ul>
      *  <li>通常需要更多的预热/测量迭代</li>
@@ -120,10 +106,8 @@ public class JMHSample_02_BenchmarkModes {
     }
 
     /**
-     * <p>
-     * {@link BenchmarkMode} 可以添加多个参数。
+     * {@link Mode} 可以添加多个参数。
      * 多个测试，一次完成。
-     * </p>
      */
 
     @Benchmark
@@ -134,9 +118,7 @@ public class JMHSample_02_BenchmarkModes {
     }
 
     /**
-     * <p>
-     * {@link Benchmark} 一次性完成所有测试。
-     * </p>
+     * {@link Mode#All} 一次性完成所有测试。
      */
     @Benchmark
     @BenchmarkMode(Mode.All)
